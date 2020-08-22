a0205bfde067281e7c25f27b0b1d8a198f2d8e61
Akash7392 <akashyesankar999@gmail.com>
2020-08-22 18:50:52 +0530

    maven complier

--- shpping_on_web/onlineweb/report.js
+++ shpping_on_web/onlineweb/report.js
@@ -82,7 +82,7 @@ formatter.examples({
   "keyword": "Examples"
 });
 formatter.before({
-  "duration": 17240251579,
+  "duration": 14952802502,
   "status": "passed"
 });
 formatter.scenario({
@@ -141,7 +141,7 @@ formatter.match({
   "location": "Buyproduct.navigate_to_website_and_click_on_sign_in_button()"
 });
 formatter.result({
-  "duration": 2479996812,
+  "duration": 3191268252,
   "status": "passed"
 });
 formatter.match({
@@ -158,21 +158,21 @@ formatter.match({
   "location": "Buyproduct.log_in_with_valid_credencials_and_and_click_on_sign_in(String,String)"
 });
 formatter.result({
-  "duration": 2119191494,
+  "duration": 1806911477,
   "status": "passed"
 });
 formatter.match({
   "location": "Buyproduct.move_the_cursor_over_women_menu_and_clicks_on_t_shrits_option()"
 });
 formatter.result({
-  "duration": 3015532703,
+  "duration": 2808939426,
   "status": "passed"
 });
 formatter.match({
   "location": "Buyproduct.more_button_will_be_displayed_then_click_on_More_button()"
 });
 formatter.result({
-  "duration": 2715385940,
+  "duration": 2462694217,
   "status": "passed"
 });
 formatter.match({
@@ -185,22 +185,22 @@ formatter.match({
   "location": "Buyproduct.increase_quantity_to_two_and_select_size_and_color_and_click_on_Add_to_cart(String)"
 });
 formatter.result({
-  "duration": 650032514,
+  "duration": 629846580,
   "status": "passed"
 });
 formatter.match({
   "location": "Buyproduct.click_Proceed_to_checkout_button()"
 });
 formatter.result({
-  "duration": 2900870039,
+  "duration": 2793564025,
   "status": "passed"
 });
 formatter.match({
   "location": "Buyproduct.complete_the_buy_order_process_till_payment()"
 });
 formatter.result({
-  "duration": 12561574347,
-  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//button[@name\u003d\u0027processAddress\u0027]\"}\n  (Session info: chrome\u003d84.0.4147.135)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-NGFOF13\u0027, ip: \u0027192.168.2.13\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_144\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 84.0.4147.135, chrome: {chromedriverVersion: 83.0.4103.39 (ccbf011cb2d2b..., userDataDir: C:\\Users\\Akash\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:58001}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:virtualAuthenticators: true}\nSession ID: 65b54e16b2e8b616db2ca4d42dd4ddf7\n*** Element info: {Using\u003dxpath, value\u003d//button[@name\u003d\u0027processAddress\u0027]}\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(Unknown Source)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(Unknown Source)\r\n\tat java.lang.reflect.Constructor.newInstance(Unknown Source)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat stepDefinitionbuyproduct.Buyproduct.complete_the_buy_order_process_till_payment(Buyproduct.java:85)\r\n\tat ✽.Then Complete the buy order process till payment(C:/Users/Akash/git/online-shopping-web/shpping_on_web/samplefeturefilefolder/Buyproduct.feature:9)\r\n",
+  "duration": 12459952758,
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//button[@name\u003d\u0027processAddress\u0027]\"}\n  (Session info: chrome\u003d84.0.4147.135)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-NGFOF13\u0027, ip: \u0027192.168.0.6\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_144\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 84.0.4147.135, chrome: {chromedriverVersion: 83.0.4103.39 (ccbf011cb2d2b..., userDataDir: C:\\Users\\Akash\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:61792}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:virtualAuthenticators: true}\nSession ID: 73e9717a486d7a3c0cbffe19115344fc\n*** Element info: {Using\u003dxpath, value\u003d//button[@name\u003d\u0027processAddress\u0027]}\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(Unknown Source)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(Unknown Source)\r\n\tat java.lang.reflect.Constructor.newInstance(Unknown Source)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat stepDefinitionbuyproduct.Buyproduct.complete_the_buy_order_process_till_payment(Buyproduct.java:85)\r\n\tat ✽.Then Complete the buy order process till payment(C:/Users/Akash/git/online-shopping-web/shpping_on_web/samplefeturefilefolder/Buyproduct.feature:9)\r\n",
   "status": "failed"
 });
 formatter.match({
@@ -210,7 +210,7 @@ formatter.result({
   "status": "skipped"
 });
 formatter.after({
-  "duration": 870138769,
+  "duration": 845842961,
   "status": "passed"
 });
 });
